22abecf210f9d8d4b7eadb57ee13c8d6feb79ce0
ericgi231 <ericgi231@gmail.com>
2020-10-09 15:18:52 -0400

    Added extra unions for other op types

--- assem.c
+++ assem.c
@@ -11,43 +11,56 @@
 #define MAXNAME 10
 #define MAXREG 5
 
-char line[MAXLINE], oper[MAXNAME], rs[MAXREG], rt[MAXREG], rd[MAXREG], imm[MAXREG], shamt[MAXREG], funct[MAXREG];
+char line[MAXLINE], oper[MAXNAME], rs[MAXREG], rt[MAXREG], rd[MAXREG], imm[MAXREG], shamt[MAXREG], funct[MAXREG], *s;
 char tempName[] = "temp.txt";
 
+//linked list node
 typedef struct Node {
     char* tag;
     int address;
     struct Node* next;
 } Node;
 
+//head node
 typedef struct {
     struct Node* first;
 } Labels;
 
-typedef struct {
-    int op;
-    int rs;
-    int rt;
-    int imm;
-} IType;
-
-typedef struct {
-    int op;
-    int imm;
-} JType;
-
+//RType data union
 union {
     unsigned int x;
     struct
     {
-        int funct:6;
-        int shamt:5;
-        int rd:5;
-        int rt:5;
-        int rs:5;
-        int op:6;
+        unsigned int funct:6;
+        unsigned int shamt:5;
+        unsigned int rd:5;
+        unsigned int rt:5;
+        unsigned int rs:5;
+        unsigned int op:6;
     } RType;
 } RTypeU;
+
+//IType data union
+union {
+    unsigned int x;
+    struct
+    {
+        unsigned int op:6;
+        unsigned int rs:5;
+        unsigned int rt:5;
+        unsigned int imm:16;
+    } IType;
+} ITypeU;
+
+//JType data union
+union {
+    unsigned int x;
+    struct
+    {
+        unsigned int op:6;
+        unsigned int imm:26;
+    } JType;
+} JTypeU;
 
 //init label
 void initLabels(Labels* labels){
@@ -112,6 +125,107 @@ char* getlabel(char* line){
 //trim label from line
 char* skiplabel(char* line){
     return strchr(line,':');
+}
+
+//convert register to value
+int registerValue(char* reg){
+    if (strcmp(reg, "zero") == 0)
+    {
+        return 0;
+    }
+    if (strcmp(reg, "at") == 0) {
+        return 1;
+    }
+    if (strcmp(reg, "v0") == 0) {
+        return 2;
+    }
+    if (strcmp(reg, "v1") == 0) {
+        return 3;
+    }
+    if (strcmp(reg, "a0") == 0) {
+        return 4;
+    }
+    if (strcmp(reg, "a1") == 0) {
+        return 5;
+    }
+    if (strcmp(reg, "a2") == 0) {
+        return 6;
+    }
+    if (strcmp(reg, "a3") == 0) {
+        return 7;
+    }
+    if (strcmp(reg, "t0") == 0) {
+        return 8;
+    }
+    if (strcmp(reg, "t1") == 0) {
+        return 9;
+    }
+    if (strcmp(reg, "t2") == 0) {
+        return 10;
+    }
+    if (strcmp(reg, "t3") == 0) {
+        return 11;
+    }
+    if (strcmp(reg, "t4") == 0) {
+        return 12;
+    }
+    if (strcmp(reg, "t5") == 0) {
+        return 13;
+    }
+    if (strcmp(reg, "t6") == 0) {
+        return 14;
+    }
+    if (strcmp(reg, "t7") == 0) {
+        return 15;
+    }
+    if (strcmp(reg, "s0") == 0) {
+        return 16;
+    }
+    if (strcmp(reg, "s1") == 0) {
+        return 17;
+    }
+    if (strcmp(reg, "s2") == 0) {
+        return 18;
+    }
+    if (strcmp(reg, "s3") == 0) {
+        return 19;
+    }
+    if (strcmp(reg, "s4") == 0) {
+        return 2;
+    }
+    if (strcmp(reg, "s5") == 0) {
+        return 21;
+    }
+    if (strcmp(reg, "s6") == 0) {
+        return 22;
+    }
+    if (strcmp(reg, "s7") == 0) {
+        return 23;
+    }
+    if (strcmp(reg, "t8") == 0) {
+        return 24;
+    }
+    if (strcmp(reg, "t9") == 0) {
+        return 25;
+    }
+    if (strcmp(reg, "k0") == 0) {
+        return 26;
+    }
+    if (strcmp(reg, "k1") == 0) {
+        return 27;
+    }
+    if (strcmp(reg, "gp") == 0) {
+        return 28;
+    }
+    if (strcmp(reg, "sp") == 0) {
+        return 29;
+    }
+    if (strcmp(reg, "fp") == 0) {
+        return 30;
+    }
+    if (strcmp(reg, "ra") == 0) {
+        return 31;
+    }
 }
 
 int main() {
@@ -184,7 +298,7 @@ int main() {
         //if not directive
         if (!(strcmp(strtok(line,"\n"), ".text") == 0 || strcmp(strtok(line,"\n"), ".data") == 0))
         {
-            char* s = skiplabel(line);
+            s = skiplabel(line);
 
             // 1. I 3 args
             //if (sscanf(s, "%10s $%5s[^,],$%5s[^,],$%d", oper, rt, rs, imm) == 4)
